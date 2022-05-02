0dbc204f976cf950794a685befa74c4d24ad9b26
saiprakash07 <88754277+saiprakash07@users.noreply.github.com>
2022-05-02 14:44:29 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+data "azurerm_kubernetes_cluster" "credentials" {
+  name                = "example-aks1"
+  resource_group_name = "rg1"
+}
+
+
+provider "helm" {
+
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+
+  }
+}
+
+
+
+
+module "prometheus-grafana" {
+
+  source = "https://github.com/saiprakash07/prometheus-grafana"
+
+}
